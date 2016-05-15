6895f62c26d7fa82a6928e182f2c3f1a922aac4f
Joel Darling <joel.darling@gmail.com>
2016-05-15 15:30:03 -0400

    synced seizures

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var colors = ['red','blue','black','purple'];
+
 server.listen(3000, function(){
   console.log('listening on port 3000');
 });
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/change_color.html');
 });
 
-io.on('connection', function (socket) {
 
-  socket.emit('color', { color: '#4a5181' });
+setInterval(function(){
+
+  var rand = Math.floor(Math.random() * 4);
+
+  io.emit('changecolor', { color: colors[rand] });
+
+},300);
+
+
+
+io.on('connection', function (socket) {
 
   console.log('user connected');
 
